78310757a34b6e3fd7d75ae602a25f00a5cdab26
Sonika Rundla <sonikarundla2023@gmail.com>
2026-02-07 16:58:46 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,24 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env");
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+let cached = global.mongoose || { conn: null, promise: null };
 
 export default async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    });
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI)
+      .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
--- modules/User.js
+++ modules/User.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-    {
-        _id: {type: String, required: true},
-        name: {type: String, required: true},
-        email: {type: String, required: true},
-        image: {type: String, required: true}
-    },
-    {timestamps: trusted}
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.models.User || mongoose.model("User",UserSchema)
-
-export default User;
+// ✅ Prevent model overwrite on Vercel
+export default mongoose.models.User ||
+  mongoose.model("User", UserSchema);

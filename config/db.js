83536b6c44a12ea5baab7b6b7cdf3e0f5fd33442
Sonika Rundla <sonikarundla2023@gmail.com>
2026-02-07 15:59:29 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose || {com: null, promise: null};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export default async function connectDB(){
-    if(cached.conn) return cached.conn;
-    if(!cached.promise){
-        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose);
-    }
-    try{
-        cached.conn = await cached.promise;
-    } catch (error) {
-           console.error("Error connecting to MongoDB :",error);
-        }
-        return cached.conn
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in .env");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export default async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }

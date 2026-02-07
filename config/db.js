49001e703a1104b7c3ef121256179f77d90649c2
Sonika Rundla <sonikarundla2023@gmail.com>
2026-02-07 17:37:03 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,27 @@
+// import mongoose from "mongoose";
+
+// let cached = global.mongoose || { conn: null, promise: null };
+
+// export default async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(process.env.MONGODB_URI)
+//       .then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
 import mongoose from "mongoose";
 
-let cached = global.mongoose || { conn: null, promise: null };
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
 
-export default async function connectDB() {
-  if (cached.conn) return cached.conn;
+  await mongoose.connect(process.env.MONGODB_URI);
+};
 
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(process.env.MONGODB_URI)
-      .then((mongoose) => mongoose);
-  }
+export default connectDB;
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  _id: String,
+  email: String,
+  name: String,
+  image: String,
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
